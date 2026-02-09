cd0a87400c04fae6d12c358f1ca42fe075be4d8c
2611saurabh <saurabh.kr026@gmail.com>
2026-02-09 20:52:15 +0530

    Microservices Section_2_1

---
+++ Backend/MicroServices/Section_2/accounts/src/main/java/com/bank/accounts/controller/AccountsController.java
@@ -0,0 +1,15 @@
+package com.bank.accounts.controller;
+
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountsController {
+
+    @GetMapping("sayHello")
+    public String hello(){
+        return "Hello E veryOne";
+    }
+}
