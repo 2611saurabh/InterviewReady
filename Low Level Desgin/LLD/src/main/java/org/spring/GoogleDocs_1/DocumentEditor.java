ce23ee6ea5bb6b723c82efaf73de2e027597485d
2611saurabh <saurabh.kr026@gmail.com>
2026-03-08 20:32:27 +0530

    update

---
+++ Low Level Desgin/LLD/src/main/java/org/spring/GoogleDocs_1/DocumentEditor.java
@@ -0,0 +1,22 @@
+package org.spring.GoogleDocs_1;
+
+import java.util.List;
+
+public class DocumentEditor {
+
+    private List<String> element;
+    private String renderedDocument;
+
+    //method or function to perform
+    public void addTest(String text){
+        element.add(text);
+    }
+
+    public void addImg(String path){
+        element.add(path);ca
+    }
+
+    public String saveToFile(){
+
+    }
+}
